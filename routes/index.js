922329588868afa2bf82cf18601279edb5dae5d3
darshan marathe <darshanmarathe@gmail.com>
2019-09-24 17:44:56 +0530

    done login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: './data/users.db', autoload: true });
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ALZ Todo App' });
@@ -13,10 +17,26 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  console.log(req.body)
-  res.render('login', { title: 'ALZ Todo App - Login' });
+  var user = req.body;
+
+  db.find({ email: user.email , pass : user.pass }, function (err, docs) {
+    console.log('docs' , docs);
+    if(docs.length > 0){
+      res.redirect("/TodosPage")
+    }else{
+      res.render('login', { message: 'Sorry Login Failed' });
+
+    }
+    // docs is an array containing documents Mars, Earth, Jupiter
+    // If no document is found, docs is equal to []
+  });
 });
 
+
+/* GET home page. */
+router.get('/TodosPage', function(req, res, next) {
+  res.render('TodosPage', { title: 'Welcome to TODO Page' });
+});
 
 /* GET home page. */
 router.get('/register', function(req, res, next) {
@@ -26,7 +46,13 @@ router.get('/register', function(req, res, next) {
 
 /* GET home page. */
 router.post('/register', function(req, res, next) {
-  console.log(req.body);
+  
+  var doc  = req.body;
+  delete doc.conpass
+  db.insert(doc, function (err, newDoc) {   // Callback is optional
+    // newDoc is the newly inserted document, including its _id
+    // newDoc has no key called notToBeSaved since its value was undefined
+  });
   res.render('register', { title: 'ALZ Todo App- Register' });
 });
 
