8b7f97a3551cd7a1b314850634a34201428a082b
darshan marathe <darshanmarathe@gmail.com>
2019-09-24 16:01:38 +0530

    added basic layout and login pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ALZ Todo App' });
+});
+
+
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'ALZ Todo App - Login' });
+});
+
+
+/* GET home page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'ALZ Todo App- Register' });
 });
 
 module.exports = router;
