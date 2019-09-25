08e4f8be8e56c1dad247239e20f8c50c9f98e10d
darshan marathe <darshanmarathe@gmail.com>
2019-09-25 11:07:31 +0530

    todo Api and login page converted to ajax

--- routes/todo.js
+++ routes/todo.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 
 var Datastore = require('nedb')
-  , db = new Datastore({ filename: './data/users2.db' , autoload: true });
+  , db = new Datastore({ filename: './data/todos.db' , autoload: true });
 
 
+//TODO : we need to fix below 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
   db.find({}, function (err, docs) {
         res.json(docs);
   });
@@ -16,13 +16,12 @@ router.get('/', function(req, res, next) {
 
 
 /* GET users listing. */
-router.get('/:email', function(req, res, next) {
-  var email = req.params.email;
-  db.find({email : email}, function (err, docs) {
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  db.find({_id : id}, function (err, docs) {
         res.json(docs);
   });
 });
-
 
 router.post('/' , function(req, res, next) {
   db.insert(req.body, function (err, newDoc) {   // Callback is optional
@@ -30,24 +29,11 @@ router.post('/' , function(req, res, next) {
   });
 })
 
-router.post('/login' , function(req, res, next) {
-  var user =  req.body
- 
-  db.find({email : user.email , pass: user.pass}, function (err, docs) {   // Callback is optional
-   if(docs.length > 0) {
-    res.json({login: true, username : user.email , loggedinTime : new Date()});
-    return;
-   }else{
-    res.json({login: false});
-   }
-  });
-})
-
 
 router.put('/' , function(req, res, next) {
-  var user = req.body;
-  db.update({_id : user._id} , user , function (err, numReplaced) {   // Callback is optional
-   res.json(user)
+  var todo = req.body;
+  db.update({_id : todo._id} , todo , function (err, numReplaced) {   // Callback is optional
+   res.json(todo)
   });
 })
 
