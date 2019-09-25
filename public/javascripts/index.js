faec7a0e68fe470f4cc5fe654de0327bb289678a
darshan marathe <darshanmarathe@gmail.com>
2019-09-25 14:01:52 +0530

    did register form also

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,3 +14,19 @@ async function postData(url = '', data = {}) {
     });
     return await response.json(); // parses JSON response into native JavaScript objects
   }
+
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
